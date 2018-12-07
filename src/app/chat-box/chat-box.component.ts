11a51683680e487516ccf3efefa25fd3d422e49f
François Nespoulous <francois.nespoulous@etu.unice.fr>
2018-12-07 04:03:09 +0100

    add answers chat box

--- src/app/chat-box/chat-box.component.ts
+++ src/app/chat-box/chat-box.component.ts
@@ -11,6 +11,7 @@ export class ChatBoxComponent implements OnInit {
   public connectedUserToken: string;
   public messageContent: string;
   public class: string;
+  public model
 
   constructor() {
     this.connectedUserToken = 'OK';
@@ -19,6 +20,23 @@ export class ChatBoxComponent implements OnInit {
 
   ngOnInit() {
     this.getMessages();
+    this.model = this.createmodel();
+  }
+
+  answer() {
+    for (const key in this.model) {
+      let re = RegExp(key);
+      if (key !== '^[a-z]') {
+        re = RegExp(key, 'i');
+      }
+      if (re.test(this.messageContent)) {
+        this.messages.push({message :
+            this.model[key][Math.floor(Math.random() * (this.model[key].length))], time: Date.now(), who: 'Bot'});
+        return;
+      }
+    }
+    alert(null);
+    return;
   }
 
   close() {
@@ -31,6 +49,7 @@ export class ChatBoxComponent implements OnInit {
 
   sendMessage() {
     this.messages.push({message : this.messageContent, time: Date.now(), who: 'User'});
+    this.answer();
     this.messageContent = '';
   }
 
@@ -44,6 +63,47 @@ export class ChatBoxComponent implements OnInit {
       time : Date.now(),
       who : 'User'
     }, ];
+  }
+
+  createmodel() {
+    return {
+      '^$': [
+        'Vous êtes censé me parler...',
+        'Une phrase est normalement composée de mots...'
+      ],
+      '^[a-z]': [
+        'Une majuscule au début d\'une phrase !'
+      ],
+      'aled|help|secours|aide|mayday': [
+        'On est jamais mieux servi que par soi-même !',
+        'Miaou',
+      ],
+      'bonne nuit': [
+        'Les machines dorment maintenant ?'
+      ],
+      'dis .*': [
+        'Ca te ferait plaisir, avoues !',
+        'Seulement si tu dis fzojvzn.'
+      ],
+      'fzojvzn': [
+        'Tu l\'as vraiment fait en plus !'
+      ],
+      'chez moi|maison|perdu': [
+        'Lis la carte !!!!',
+        'Tous les chemins mènent à Rome !!!!!!!!!!!!!!!!',
+        'TOUT DROIT',
+        'On va tous mourir !'
+      ],
+      'mort|meurt|mourir': [
+        'Omae wa mou shindeiru.'
+      ],
+      'chut|tais|insupportable': [
+        'No u.'
+      ],
+      't ki|Qui es.tu|Pr.sente': [
+        'Je suis un robot qui répond aux questions que mes développeurs ont bien voulu implémenter.'
+      ]
+    };
   }
 
 }
